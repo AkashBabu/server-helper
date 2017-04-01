7e74d7d7ca9474a34d50c65ed5989588741fae7e
Akash Babu <001akashbabu@gmail.com>
2017-04-01 16:15:49 +0530

    Changed OR type checks to undefined/checks

--- test/helperMongo/test.js
+++ test/helperMongo/test.js
@@ -20,4 +20,47 @@ describe("helper mongo", () => {
     after((done) => {
         db.dropDatabase(done)
     })
+
+    describe("#getDateFormat()", () => {
+        it("should return Year format", () => {
+            helperMongo.getDateFormat("year").should.be.eql("%Y")
+
+            var user = {
+                name: 'Akash',
+                cTime: new Date()
+            }
+
+            db.collection(userColl).insert(user, function(err, result) {
+                if(result) {
+                    db.collection
+                } else {
+                    console.log("Failed to Insert User in should return Year format");
+                    done("Failed to insert User in should return Year format")
+                }
+            })
+
+
+        })
+        it("should return Month format", () => {
+            helperMongo.getDateFormat("month").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Day format", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Hour format", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Minute format", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Second format", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Millisecond format", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+        it("should return Day format by default", () => {
+            helperMongo.getDateFormat("day").should.be.eql("%Y-%m-%d")
+        })
+    })
 })
