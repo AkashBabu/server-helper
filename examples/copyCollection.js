b28adf2fbcc4e7117e4f256e32f5b69b4cfbe1cc
AkashBabu <001akashbabu@gmail.com>
2017-07-02 23:55:34 +0530

    Removed Examples

--- examples/copyCollection.js
+++
@@ -1,9 +0,0 @@
-const mongojs = require('mongojs');
-const async = require('async');
-const helperDb = require('../lib/helperDb');
-
-var args = process.argv.slice(2);
-
-helperDb.copyCollection(args[0], args[1], args[2], args[3] || args[1], function(err, n) {
-    err ? console.log('Collection copying failed') : console.log('Collection copied successfully');
-})
