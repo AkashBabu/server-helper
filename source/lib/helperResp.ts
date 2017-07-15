835760766f5c535f18f4df71be23681cec143a7b
akashbabu <001akashbabu@gmail.com>
2017-07-16 00:02:20 +0530

    JSDoc

--- source/lib/helperResp.ts
+++ source/lib/helperResp.ts
@@ -20,18 +20,35 @@ export class HelperResp implements IHelperResp {
 
         return this;
     }
+
+    /**
+     * 401 handler
+     * @param res - Express Response object
+     * @param comment=UNAUTHORIZED ACCESS Response data
+     */
     public unauth(res: IHTTPResp, comment?: string): void {
         res.status(401).send({
             error: true,
             data: comment || "UNAUTHORIZED ACCESS"
         })
     }
+
+    /**
+     * 500 handler
+     * @param res Express Response object
+     * @param comment=INTERNAL SERVER ERROR Response data
+     */
     public serverError(res: IHTTPResp, comment?: string): void {
         res.status(500).send({
             error: true,
             data: comment || "INTERNAL SERVER ERROR"
         })
     }
+
+    /**
+     * Callback Handler
+     * @param res Express Response object
+     */
     public handleResult(res: IHTTPResp): Function {
         return (err: Error, result: any, type = "array"): void => {
             type = type.toLowerCase();
@@ -66,37 +83,74 @@ export class HelperResp implements IHelperResp {
     //         })
     //     }
     // }
+
+    /**
+     * 200 handler
+     * @param res Express Response object
+     * @param data={} Response data
+     */
     public success(res: IHTTPResp, data?: object): void {
         res.status(200).send({
             error: false,
             data: data || {}
         })
     }
+
+    /**
+     * 400 handler
+     * @param res Express Response object
+     * @param data=Failed Response data
+     */
     public failed(res: IHTTPResp, data?: string): void {
         res.status(400).send({
             error: true,
             data: data || "Failed"
         })
     }
+
+    /**
+     * Create handler
+     * @param res Express Response Object
+     * @param data=CREATED Response data
+     */
     public post(res: IHTTPResp, data?: object): void {
         res.status(201).send({
             error: false,
             data: data || "CREATED"
         })
     }
+
+    /**
+     * Update handler
+     * @param res Express Response object
+     * @param data=UPDATED Response data
+     */
     public put(res: IHTTPResp, data?: object): void {
         res.status(202).send({
             error: false,
             data: data || "UPDATED"
         })
     }
+
+    /**
+     * Delete Handler
+     * @param res Express Response object
+     * @param data=DELETED Response data
+     */
     public delete(res: IHTTPResp, data?: object): void {
         res.status(202).send({
             error: false,
             data: data || "DELETED"
         })
     }
-    public get(res: IHTTPResp, data, list = true): void {
+
+    /**
+     * Get and List Handler
+     * @param res Express Response object
+     * @param data Response data
+     * @param list=true if true then data Defaults to [] else {}
+     */
+    public get(res: IHTTPResp, data?: any, list: boolean = true): void {
         res.status(200).send({
             error: false,
             data: this.isUndefined(data) ? (list ? { count: 0, list: [] } : {}) : data
